bcbe903f91ce8aae0a327646e126f974d929d9ad
christodenny <chris.denny@utexas.edu>
2015-03-01 02:29:18 -0600

    integrated search bar with map

--- app/src/main/java/com/apps/chris/hotjobs/MapsActivity.java
+++ app/src/main/java/com/apps/chris/hotjobs/MapsActivity.java
@@ -3,11 +3,20 @@ package com.apps.chris.hotjobs;
 import java.util.*;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -22,12 +31,54 @@ import com.google.maps.android.heatmaps.WeightedLatLng;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    Button mButton;
+    EditText mEdit;
+    private String query = "";
+    TextView viewtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(40.000, -95.000), 3.0f) );
+
+        mButton = (Button) findViewById(R.id.button);
+        mEdit = (EditText) findViewById(R.id.input1);
+        viewtext = (TextView) findViewById(R.id.output1);
+        mButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        query= mEdit.
+                                getText().
+                                toString() ;
+                        //viewtext.setText(query);
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(mEdit.getWindowToken(),0);
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -74,6 +125,7 @@ public class MapsActivity extends FragmentActivity {
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
+                mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(40.000, -95.0000), 3.0f) );
                 addHeatMap();
             }
         }
